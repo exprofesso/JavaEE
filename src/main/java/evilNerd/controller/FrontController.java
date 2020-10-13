e0dbcdddd522ac305c7f93797fde52af5daf65d7
Sergey Podkolzin <35434142+exprofesso@users.noreply.github.com>
2020-10-13 13:45:14 +0300

    Add AuthHeaderFilter, CharsetFilter and listener

--- src/main/java/evilNerd/controller/FrontController.java
+++ src/main/java/evilNerd/controller/FrontController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 public class FrontController extends  HttpServlet{
     public FrontController() {
@@ -25,10 +26,18 @@ public class FrontController extends  HttpServlet{
 
     private void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/hello");
+        if (dispatcher != null) {
+            System.out.println("Forward will be done!");
+            req.setAttribute("userName", userRepository.findAll().stream().map(User::getName).collect(Collectors.joining(" , ")));
+            dispatcher.forward(req, resp);
+        }
+
+
         // Третий вариант задание номер 6
 
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
-        dispatcher.forward(req, resp);
+    //    RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+     //   dispatcher.forward(req, resp);
 
 
 
